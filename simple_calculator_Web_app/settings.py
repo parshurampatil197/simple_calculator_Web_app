2cfb46b72f7e6ed2ee39fd6d98393039f671a24a
parshurampatil197 <parshurampatil197@gmail.com>
2022-10-13 18:59:05 +0530

    wsgi chaged

--- simple_calculator_Web_app/settings.py
+++ simple_calculator_Web_app/settings.py
@@ -41,7 +41,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'django_configs.urls'
+ROOT_URLCONF = 'simple_calculator_Web_app.urls'
 
 TEMPLATES = [
     {
@@ -59,7 +59,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'django_configs.wsgi.application'
+WSGI_APPLICATION = 'simple_calculator_Web_app.wsgi.application'
 
 
 # Database
--- simple_calculator_Web_app/wsgi.py
+++ simple_calculator_Web_app/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for django_configs project.
+WSGI config for simple_calculator_Web_app project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_configs.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'simple_calculator_Web_app.settings')
 
 application = get_wsgi_application()
